8e83938450605c929c1da0065082c0037868c34a
Aqib Muhammad Aslam <73635332+Aqib-Muhammad-Aslam@users.noreply.github.com>
2021-03-26 22:51:05 +0500

    Add files via upload

---
+++ week 2/Function.js
@@ -0,0 +1,12 @@
+function myname(name)
+{
+    console.log('my name is ' +name) //for printing text on console 
+}
+myname('aqib')
+
+function c(greating){
+    console.log('Hello Class'+greating)
+}
+c('How Are You.')
+
+exports.myname = myname;
